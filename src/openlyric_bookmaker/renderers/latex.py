830febfea729c5b9e1c6896bafad3214847c5361
Adam Milner <carmiac@gmail.com>
2026-02-12 22:04:37 -0800

    Fix escaping special characters

--- src/openlyric_bookmaker/renderers/latex.py
+++ src/openlyric_bookmaker/renderers/latex.py
@@ -10,6 +10,35 @@ from openlyric_bookmaker.models.song import Song
 logger = logging.getLogger(__name__)
 
 
+def latex_escape(text: str) -> str:
+    """Escape special LaTeX characters in text.
+    
+    Args:
+        text: Text to escape
+        
+    Returns:
+        Escaped text safe for LaTeX
+    """
+    # Order matters - backslash must be done first, but we handle it differently
+    # to avoid double-escaping
+    text = text.replace('\\', r'\textbackslash{}')
+    # Now handle other special characters
+    replacements = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\textasciicircum{}',
+    }
+    for char, replacement in replacements.items():
+        text = text.replace(char, replacement)
+    return text
+
+
 class LaTeXRenderer:
     """Renders songs to LaTeX .sbd format using Jinja2 templates."""
 
@@ -44,6 +73,9 @@ class LaTeXRenderer:
                 lstrip_blocks=True,
                 autoescape=False,
             )
+        
+        # Add LaTeX escape filter
+        self.env.filters['latex_escape'] = latex_escape
 
     def render_song(self, song: Song) -> str:
         """Render a single song to LaTeX .sbd format.
